e532526d82e6a7126622a2fb497136f68d9db697
Thananat <youshaarif13@gmail.com>
2023-01-24 16:06:37 +0700

    almost

--- src/components/dashboard/DashboardFeed.jsx
+++ src/components/dashboard/DashboardFeed.jsx
@@ -1,5 +1,24 @@
-import { useState } from "react";
+import moment from "moment/moment";
+import { useEffect, useState } from "react";
 import pb from "../../lib/pocketbase";
+import { getImageURL } from "../../lib/utils";
+import React from "react";
+import { Dropdown, Space } from "antd";
+
+const items = [
+  {
+    label: <a href="https://www.antgroup.com">copy</a>,
+    key: "0",
+  },
+  {
+    type: "divider",
+  },
+  {
+    label: "delete",
+    key: "3",
+  },
+];
+
 function DashboardFeed() {
   const data = {
     name: "test",
@@ -9,19 +28,30 @@ function DashboardFeed() {
     image: "null",
   };
   const [file, setFile] = useState();
-  const [image, setImage] = useState();
+  const [imageList, setImageList] = useState(null);
 
-  // function handleChange(event) {
-  //   setFile(event.target.files[0]);
-  // }
-  // console.log(file);
+  useEffect(() => {
+    fetchImageData();
+  }, []);
+
+  const fetchImageData = async (event) => {
+    try {
+      const resultList = await pb.collection("upload").getList(1, 20);
+      pb.autoCancellation(false);
+      console.log(resultList);
+      setImageList(resultList);
+    } catch (e) {
+      alert(e);
+    }
+  };
 
   async function handleChange(event, image) {
-    setFile(event.target.files[0]);
-    console.log(file);
+    const formData = new FormData();
+    formData.append("field", event.target.files[0]);
+    formData.append("title", event.target.files[0].name);
+
     try {
-      const record = await pb.collection("tools").create(image);
-      console.log(image);
+      const record = await pb.collection("upload").create(formData);
     } catch (e) {
       alert(e);
     }
@@ -31,51 +61,64 @@ function DashboardFeed() {
     <div>
       <div className="flex items-center justify-center w-full p-4">
         <label className="flex flex-col items-center justify-center w-full p-4 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100 ">
-          <div className="flex flex-col items-center justify-center ">
-            <p className="text-sm text-gray-500 dark:text-gray-400">
+          <div className="flex flex-col items-center justify-center py-2">
+            <p className="text-sm text-gray-500">
               <span className="font-semibold">CLICK TO ADD NEW</span>
             </p>
           </div>
           <input
             type="file"
             className="hidden"
-            name="file"
-            value={image}
-            onChange={handleChange}
+            onChange={(event) => handleChange(event)}
           />
         </label>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-4 p-4">
-        <div className="flex justify-between items-center bg-white p-2 border rounded-md">
-          <div className="flex">
+      <div className="grid grid-cols-1 md:grid-cols-4 p-4 ">
+        {imageList?.items.map((data, index) => (
+          <div
+            key={index}
+            className="flex justify-between items-center bg-white p-2 border rounded-md mr-2 mb-2"
+          >
             <img
-              src="https://ggsc.s3.amazonaws.com/images/made/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner_300_200_int_c1-1x.jpg"
+              src={getImageURL(data.collectionId, data.id, data.field, 100)}
               alt=""
               className="w-10 h-10 mr-2"
             />
             <div className="">
-              <p className="font-semibold text-sm">IMG_1234.jpg</p>
-              <p className="text-xs text-gray-400">11 Jan 2012</p>
-              {/* <p>{currentUser}</p> */}
+              <p className="font-semibold text-sm">{data.title}</p>
+              <p className="text-xs text-gray-400">
+                {moment(data.created).format("DD/MM/YYYY")}
+              </p>
             </div>
+            <Dropdown
+              className=" flex"
+              menu={{
+                items,
+              }}
+              trigger={["click"]}
+            >
+              <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    className="w-5 h-5 cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                    />
+                  </svg>
+                </Space>
+              </a>
+            </Dropdown>
           </div>
-
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            className="w-5 h-5 cursor-pointer"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-            />
-          </svg>
-        </div>
+        ))}
       </div>
     </div>
   );
