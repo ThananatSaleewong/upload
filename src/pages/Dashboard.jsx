8eaa93dfc2f63567e9f2e51eb1802252b10308f5
Thananat <youshaarif13@gmail.com>
2023-02-16 16:58:21 +0700

    CHANGE: Merge header components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import DashboardFeed from "../components/dashboard/DashboardFeed";
-// import DashboardHeader from "../components/dashboard/DashboardHeader";
 
 import pb from "../lib/pocketbase";
 
@@ -18,7 +17,21 @@ function Dashboard() {
 
   return (
     <div>
-      {/* <DashboardHeader logout={logout} /> */}
+      <header className="flex justify-between px-4 py-2 bg-white border-b items-center">
+        <div className="flex justify-center items-center space-x-2 cursor-pointer">
+          <img src="/image/triangle-logo.png" alt="" className="w-6 h-6" />
+          <h1 className="text-2xl font-bold">Spaces</h1>
+        </div>
+        <div className="flex items-center font-medium">
+          <p>{currentUser.model.name}</p>
+          <button
+            onClick={logout}
+            className="p-4 bg-red-600 text-white font-semibold rounded-md"
+          >
+            Logout
+          </button>
+        </div>
+      </header>
       <DashboardFeed currentUser={currentUser} />
     </div>
   );
