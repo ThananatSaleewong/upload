eac9440eded3de4508708a1c367925ee8109689f
thananat@office <thananat.s25@gmail.com>
2023-03-10 17:58:51 +0700

    CHANGE CSS margin

--- src/components/login/LoginHeader.jsx
+++ src/components/login/LoginHeader.jsx
@@ -1,8 +1,6 @@
-
-
 function LoginHeader() {
   return (
-    <header className="flex justify-center items-center space-x-2 mb-10">
+    <header className="flex justify-center items-center space-x-2 mb-4">
       <img src="/image/triangle-logo.png" alt="" className="w-6 h-6" />
       <h1 className="text-2xl font-bold">Spaces</h1>
     </header>
